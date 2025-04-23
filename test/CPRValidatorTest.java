25e37696bf24b28f19299b40eea992d7347d372e
nikholt1 <niklas1997@live.dk>
2025-04-23 17:10:53 +0200

    CPRValidatorTest isValid and isValidDate test implementations

--- test/CPRValidatorTest.java
+++ test/CPRValidatorTest.java
@@ -17,6 +17,39 @@ class CPRValidatorTest {
         c = null;
     }
 
+    // isValid method tests
+    @Test
+    void isValid_test_Over_10_Characters_To_Return_False() {
+        assertFalse(c.isValid("03209502191"));
+    }
+    @Test
+    void isValid_test_Under_10_Characters_To_return_False() {
+        assertFalse(c.isValid("032095021"));
+    }
+    @Test
+    void isValid_test_Right_On_10_to_Return_true() {
+        assertTrue(c.isValid("0320950219"));
+    }
+
+
+
+
+    // isValidDate method tests
+    @Test
+    void isValidDate_test_date_in_Future_to_return_false() {
+        assertFalse(c.isValidDate("0320260219"));
+    }
+
+    @Test
+    void isValidDate_test_date_not_in_future_to_return_true() {
+        assertTrue(c.isValidDate("0320950219"));
+    }
+
+    // TODO
+    ///implement:
+    //isValidModulus method tests
+
+
 
 
 }
