e2acc92894107f6e9e858937f186f2bdfd2fa8ad
nikholt1 <niklas1997@live.dk>
2025-04-23 16:25:48 +0200

    CPRValidatorTest.java starefore each and AfterEach annotations

--- test/CPRValidatorTest.java
+++ test/CPRValidatorTest.java
@@ -1,7 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CPRValidatorTest {
 
+    private CPRValidator c;
+    @BeforeEach
+    void startUp() {
+        c = new CPRValidator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        c = null;
+    }
 
 
 }
